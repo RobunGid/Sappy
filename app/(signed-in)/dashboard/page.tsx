925234d9a7c41c96d7cc9236dc3e67fe2c3454d3
RobunGid <robungidorig@gmail.com>
2025-12-12 20:01:59 +0300

    start dashboard

--- app/(signed-in)/dashboard/page.tsx
+++ app/(signed-in)/dashboard/page.tsx
@@ -1,3 +1,15 @@
+'use client';
+
+import { useSidebar } from '@/components/ui/sidebar';
+import { useUser } from '@clerk/nextjs';
+import { useRouter } from 'next/navigation';
+import { useChatContext } from 'stream-chat-react';
+
 export default function Dashboard() {
+	const { user } = useUser();
+	const router = useRouter();
+	const { channel, setActiveChannel } = useChatContext();
+	const { setOpen } = useSidebar();
+
 	return <div>Dashboard</div>;
 }
